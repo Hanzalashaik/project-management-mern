43af3d12fa9a9715766e3e083bc234233eb414b3
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-01-11 15:35:49 +0000

    Minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,17 +21,17 @@ export default function App() {
     <UserProvider>
       <Router>
 
-
-        <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
-        <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
-        <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <Signup />} />
-        <Route path="/user" element={isAuthenticated ? <User /> : <Navigate to="/login" />} />
-        <Route path="/admin" element={admin ? <Admin /> : <Navigate to="/login" />} />
-        <Route path="/forgetpassword" element={isAuthenticated ? <Navigate to="/" /> : <ForgetPassword />} />
-        <Route path="/profile" element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" />} />
-        <Route path="/edit" element={isAuthenticated ? <EditPage /> : <Navigate to="/login" />} />
-        <Route path="/reset-password" element={isAuthenticated ? <ResetPassword /> : <Navigate to="/login" />} />
-
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
+          <Route path="/signup" element={isAuthenticated ? <Navigate to="/" /> : <Signup />} />
+          <Route path="/user" element={isAuthenticated ? <User /> : <Navigate to="/login" />} />
+          <Route path="/admin" element={admin ? <Admin /> : <Navigate to="/login" />} />
+          <Route path="/forgetpassword" element={isAuthenticated ? <Navigate to="/" /> : <ForgetPassword />} />
+          <Route path="/profile" element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" />} />
+          <Route path="/edit" element={isAuthenticated ? <EditPage /> : <Navigate to="/login" />} />
+          <Route path="/reset-password" element={isAuthenticated ? <ResetPassword /> : <Navigate to="/login" />} />
+        </Routes>
       </Router>
     </UserProvider>
   );
